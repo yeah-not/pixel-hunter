a8374d2c6e1ca364d72c39f223504465f37a816e
yeah-not <yeahhh.not@gmail.com>
2021-05-06 11:53:34 +0300

    Добавляет обработчик на нажатие ← и → для переключения экранов. Улучшает функцию вывода экрана.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const mainEl = document.querySelector(`#main`);
 const SCREENS = [
   document.querySelector(`#intro`).content,
   document.querySelector(`#greeting`).content,
@@ -20,9 +19,37 @@ const removeChildren = (el, selector) => {
   }
 };
 
+const mainEl = document.querySelector(`#main`);
+let currentScreen = 0;
+
 const showScreen = (index) => {
+  if (index >= SCREENS.length - 1) {
+    index = 0;
+  } else if (index < 0) {
+    index = SCREENS.length - 1;
+  }
+
   removeChildren(mainEl);
   mainEl.appendChild(SCREENS[index].cloneNode(true));
+
+  currentScreen = index;
 };
+
+document.addEventListener(`keydown`, (evt) => {
+  if (evt.altKey === true || evt.ctrlKey === true || evt.shiftKey === true) {
+    return;
+  }
+
+  switch (evt.code) {
+    case `ArrowRight`:
+      evt.preventDefault();
+      showScreen(currentScreen + 1);
+      break;
+    case `ArrowLeft`:
+      evt.preventDefault();
+      showScreen(currentScreen - 1);
+      break;
+  }
+});
 
 showScreen(0);
