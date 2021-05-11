84b0a0e6b8ba05ec3e8cff5aad363466b9299fac
yeah-not <yeahhh.not@gmail.com>
2021-05-11 11:21:16 +0300

    Разбивает на модули шаблоны экранов игры из index.html

---
+++ js/screen-intro.js
@@ -0,0 +1,14 @@
+// Игровой экран - Интро
+// ---------------------
+
+import {getDomElement} from './util.js';
+
+const template = `
+  <section class="intro">
+    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+  </section>
+`;
+const element = getDomElement(template);
+
+export default element;
